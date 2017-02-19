39c0a2333f4221468cc79787976c8d4c99fc84e8
Patrick Ekel <patrick.ekel@utah.edu>
2017-02-18 23:35:42 -0700

    Animation about done

--- core/src/com/mygdx/game/Tappy_Race.java
+++ core/src/com/mygdx/game/Tappy_Race.java
@@ -13,30 +13,37 @@ public class Tappy_Race extends ApplicationAdapter {
 	Texture[] lights = new Texture[3];
 	Texture redLight, yellowLight, greenLight;
 	long startTime;
-	float totalTime = 3.5f;
-	int numClicks = 1;
-	boolean gameStarted = false;
+	float totalTime;
+	int numClicks;
+	boolean gameStarted;
 	int i = 0;
-	boolean inPreRace = true;
-	float carY = 0;
-	float velocity = 0;
-	int gameState = 0;
-	float drag = 0;
+	boolean inPreRace;
+	float carY;
+	float velocity;
+	int gameState;
+	float drag;
+	float deltaY;
+	float lastY;
+	float changeInY;
+	float finishDistance;
 	float width;
 	float height;
 	float carWidth;
 	float carHeight;
+	float currentCarWidth;
+	float currentCarHeight;
 	float finishLine;
 
-
-
 	@Override
-	public void create() {   /////////////// INSTANTIATE ALL VARIABLES HEREEEEE not UP TOPPPPPPP
-		inPreRace = true;		//////////  "HARD" code car dimensions and adjust based on num clicks/ new xWid yWidth in that location......switch statement!@!///
+	public void create() {
+		inPreRace = true;
  		gameStarted = false;
 		batch = new SpriteBatch();
 		totalTime = 3.5f;
 		carWidth = 0;
+		velocity = 0;
+		gameState = 0;
+		drag = 0;
 		background = new Texture("road_into.jpg");
 		finish = new Texture("finish_removed.jpg");
 		car = new Texture("orangeSportCar.jpg");
@@ -48,12 +55,16 @@ public class Tappy_Race extends ApplicationAdapter {
 		lights[2] = greenLight;
 		startTime = 0;
 		numClicks = 1;
+		lastY = 135;
+		deltaY = 0;
+		changeInY = 0;
+		currentCarWidth = 4.5f;
+		currentCarHeight = 4.5f;
 		finishLine = Gdx.graphics.getHeight()/2;
 		start();
 	}
 
 	public void start() {
-		//carY = (Gdx.graphics.getHeight() / 2 - car.getHeight() / 8); //new Timer().scheduleAtFixedRate(task, after, interval)
 		carY = 135;
 		gameState = 1;
 		inPreRace = true;
@@ -63,8 +74,10 @@ public class Tappy_Race extends ApplicationAdapter {
 		gameStarted = false;
 		width = Gdx.graphics.getWidth();
 		height = Gdx.graphics.getHeight();
-		carWidth = width / 4.5f;
-		carHeight = height / 4.5f;
+		carWidth = width / currentCarWidth;
+		carHeight = height / currentCarWidth;
+		finishDistance = height / 28;
+		numClicks = 1;
 
 	}
 
@@ -73,7 +86,7 @@ public class Tappy_Race extends ApplicationAdapter {
 		batch.begin();
 		if (inPreRace) {
 			batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-			batch.draw(car, (Gdx.graphics.getWidth() / 2 - carWidth/2), 135, carWidth, carHeight);
+			batch.draw(car, (Gdx.graphics.getWidth() / 2 - carWidth/2), carY, carWidth, carHeight);
 			batch.draw(finish, 655, 1280, Gdx.graphics.getWidth() / 11, Gdx.graphics.getWidth() / 12);
 			if (TimeUtils.timeSinceNanos(startTime) > 1000000000 && i < 3) {
 				batch.draw(lights[i], 522, 1440, Gdx.graphics.getWidth() / 4, Gdx.graphics.getHeight() / 4);
@@ -83,128 +96,43 @@ public class Tappy_Race extends ApplicationAdapter {
 				gameStarted = true;
 				inPreRace = false;
 				gameState = 1;
-				velocity = 0;//200;
+				velocity = 1;
 			} else if (i < 3 && TimeUtils.timeSinceNanos(startTime) < 1000000000) {
 				batch.draw(lights[i], 522, 1440, Gdx.graphics.getWidth() / 4, Gdx.graphics.getHeight() / 4);
 			}
 		} else if (gameStarted) {
-			//setCarMovement();
-			velocity = drag;
 			if (gameState == 1) {
 				if (Gdx.input.justTouched()) {
 					numClicks++;
-					//setCarMovement();
 					carY += velocity;
+					velocity+=.55;
 				}
-				if (carY < finishLine) {
-					setCarMovement();
+				if (carY < finishLine + finishDistance) {
 					velocity += drag;
 					carY += velocity;
-
+					setCarMovement();
 				} else {
 					gameState = 2;
-				}//gameState=0;
+				}
 			} else if (gameState == 0) {
 				if (Gdx.input.justTouched()) {
 					gameState = 1;
 				}
-			} else if (gameState == 2) {								//////////////////											!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-				//GAME OVER win winnnnderrr?                                            !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
-			}																//// gonnan need a switch here of some sort...to determine car width and height
-			batch.draw(finish, 655, 1280, Gdx.graphics.getWidth() / 11, Gdx.graphics.getWidth() / 12);
+			} else if (gameState == 2) {
+				//GAME OVER win winnnnderrr?
+			}
 			batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+			batch.draw(finish, 655, 1280, Gdx.graphics.getWidth() / 11, Gdx.graphics.getWidth() / 12);
 			batch.draw(car, (Gdx.graphics.getWidth() / 2 - carWidth/2), carY, carWidth, carHeight);
 		}
 		batch.end();
 	}
-
-
+	
 	public void setCarMovement(){
-		if (carY < height / 6){
-			drag = .045f;velocity = 4;carWidth *=.998;/// 4.6f;
-			carHeight*=.998; //return;
-		}
-		else if (carY < height / 5){
-			drag = .075f;velocity = 4.5f;carWidth *=.995;/// 4.6f;
-			carHeight*=.995;
-		}
-		else if (carY < height / 4){
-			drag = .125f;velocity = 4.5f;carWidth *=.992;/// 4.6f;
-			carHeight*=.992;
-		}
-		else if (carY < height / 3){
-			drag = .145f;velocity = 4.75f;carWidth *=.988;/// 4.6f;
-			carHeight*=.986;
-		}
-		else{
-			drag = .175f;velocity = 5.2f;carWidth *=.979;/// 4.6f;
-			carHeight*=.975;
-		}
-		/**
-		if (carY < finishLine/3){  // drag a push? .. change to momentum..
-			drag = .075f;velocity = 5;carWidth *=.98;/// 4.6f;
-			carHeight*=.98; return;}// = height / 4.6f;return;	}
-		else if (carY < finishLine/2.5){
-			drag = .075f;velocity = 5;carWidth*= .98;// height / 4.8f;
-			carHeight *= .98;return;}//4.8f;return;	}
-		else if (carY < finishLine/2){
-			drag = .1f;velocity = 6;carWidth = width / 5.1f;
-			carHeight = height / 5.1f;return;	}
-		else if (carY < finishLine/1.8){
-			drag = .15f;velocity = 6;carWidth = width / 5.4f;
-			carHeight = height / 5.4f;return;	}
-		else if (carY < finishLine/1.6){
-			drag = .25f;velocity = 7;carWidth = width / 5.8f;
-			carHeight =  height/ 5.8f;return;	}
-		else if (carY < finishLine/1.4){
-			drag = .3f;velocity = 7;carWidth = width / 6.3f;
-			carHeight = height / 6.3f;return;	}
-		else if (carY < finishLine/1.2){
-			drag = .35f;velocity = 8;carWidth = width / 6.8f;
-			carHeight = height / 6.8f;return;	}
-		else if (carY < finishLine/1.1){
-			drag = .4f;velocity = 8;carWidth = width / 7.5f;
-			carHeight = height / 7.5f;return;	}
-		else if (carY < finishLine/1.08){
-			drag = .5f;velocity = 8;carWidth = width / 8.5f;
-			carHeight = height / 8.5f;return;	}
-		else if (carY < finishLine/1.05){
-			drag = .6f;velocity = 8;carWidth = width/10;
-			carHeight = height /10;return;	}
-		else if (carY < finishLine/1.05){
-			drag = .6f;velocity = 8;carWidth = width/12;
-			carHeight = height/12;return;	}
-		else if (carY < finishLine/1.05){
-			drag = .6f;velocity = 8;carWidth = width/14;
-			carHeight = height/14;return;	}
-		else if (carY < finishLine/1.05){
-			drag = .6f;velocity = 8;carWidth = width/15;
-			carHeight = height/13;return;	}
-		else if (carY < finishLine/1.05){
-			drag = .6f;velocity = 8;carWidth = width/17;
-			carHeight = height/17;return;	}
-		else if (carY < finishLine/1.05){
-			drag = .6f;velocity = 8;carWidth = width/20;
-			carHeight = height/20;return;	}
-		else if (carY < finishLine/1.05){
-			drag = .6f;velocity = 8;carWidth = width/22;
-			carHeight = height/22;return;	}
-		else if (carY < finishLine/1.05){
-			drag = .6f;velocity = 8;carWidth = width/25;
-			carHeight = height/25;return;	}
-		else if (carY < finishLine/1.05){
-			drag = .6f;velocity = 8;carWidth = width/28;
-			carHeight = height/28;return;	}
-		else if (carY < finishLine/1.05){
-			drag = .6f;velocity = 8;carWidth = width/32;
-			carHeight = height/32;return;	}
-
-		else{
-			drag = .75f;velocity = 8;carWidth = width/35;
-			carHeight = height/35;
-			//System.out.println(Gdx.graphics.getHeight());
-
-		 }**/
+		System.out.println("CarY is " + (carY - 135));
+		deltaY = (carY - 135)*.0075f;
+		carWidth = width / (4.5f + ((deltaY*deltaY*deltaY)*.055f));
+		carHeight = height / (4.5f + ((deltaY*deltaY*deltaY)*.055f));
 	}
 
 		@Override
