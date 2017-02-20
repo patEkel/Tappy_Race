017fc2a3b85992777efc6b6b0dae1b1ef22cd543
Patrick Ekel <patrick.ekel@utah.edu>
2017-02-19 23:24:56 -0700

    Adding timer. Need to get decimal fixed

--- core/src/com/mygdx/game/Tappy_Race.java
+++ core/src/com/mygdx/game/Tappy_Race.java
@@ -9,30 +9,19 @@ import com.badlogic.gdx.utils.TimeUtils;
 
 public class Tappy_Race extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture background, finish, car;
+	Texture background, finish, orangeCar, whiteCar, redCar, greenCar;
 	Texture[] lights = new Texture[3];
 	Texture redLight, yellowLight, greenLight;
-	long startTime;
+	long countDownTime,raceStartTime, raceTime;
 	float totalTime;
 	int numClicks;
 	boolean gameStarted;
 	int i = 0;
-	boolean inPreRace;
-	float carY;
-	float velocity;
 	int gameState;
-	float drag;
-	float deltaY;
-	float lastY;
-	float changeInY;
-	float finishDistance;
-	float width;
-	float height;
-	float carWidth;
-	float carHeight;
-	float currentCarWidth;
-	float currentCarHeight;
-	float finishLine;
+	boolean inPreRace;
+	float carY, deltaY, drag, velocity;
+	float finishLine, finishDistance;
+	float width, height, carWidth, carHeight;
 
 	@Override
 	public void create() {
@@ -46,20 +35,17 @@ public class Tappy_Race extends ApplicationAdapter {
 		drag = 0;
 		background = new Texture("road_into.jpg");
 		finish = new Texture("finish_removed.jpg");
-		car = new Texture("orangeSportCar.jpg");
+		orangeCar = new Texture("orangeSportCar.jpg");
 		redLight = new Texture("red_light.jpg");
 		yellowLight = new Texture("yellow_light.jpg");
 		greenLight = new Texture("green_light.jpg");
 		lights[0] = redLight;
 		lights[1] = yellowLight;
 		lights[2] = greenLight;
-		startTime = 0;
+		countDownTime = 0;
+		raceStartTime = 0;
 		numClicks = 1;
-		lastY = 135;
 		deltaY = 0;
-		changeInY = 0;
-		currentCarWidth = 4.5f;
-		currentCarHeight = 4.5f;
 		finishLine = Gdx.graphics.getHeight()/2;
 		start();
 	}
@@ -70,75 +56,78 @@ public class Tappy_Race extends ApplicationAdapter {
 		inPreRace = true;
 		i = 0;
 		drag = 0;
-		startTime = TimeUtils.nanoTime();
+		countDownTime = TimeUtils.nanoTime();
 		gameStarted = false;
 		width = Gdx.graphics.getWidth();
 		height = Gdx.graphics.getHeight();
-		carWidth = width / currentCarWidth;
-		carHeight = height / currentCarWidth;
+		carWidth = width / 4.5f;
+		carHeight = height / 4.5f;
 		finishDistance = height / 28;
 		numClicks = 1;
-
 	}
 
 	@Override
 	public void render() {
 		batch.begin();
-		if (inPreRace) {
+		if (inPreRace) { // three second countdown..tapps are not yet valid
 			batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-			batch.draw(car, (Gdx.graphics.getWidth() / 2 - carWidth/2), carY, carWidth, carHeight);
+			batch.draw(orangeCar, (Gdx.graphics.getWidth() / 2 - carWidth/2), carY, carWidth, carHeight);
 			batch.draw(finish, 655, 1280, Gdx.graphics.getWidth() / 11, Gdx.graphics.getWidth() / 12);
-			if (TimeUtils.timeSinceNanos(startTime) > 1000000000 && i < 3) {
+			if (TimeUtils.timeSinceNanos(countDownTime) > 1000000000 && i < 3) {
 				batch.draw(lights[i], 522, 1440, Gdx.graphics.getWidth() / 4, Gdx.graphics.getHeight() / 4);
-				startTime = TimeUtils.nanoTime();
+				countDownTime = TimeUtils.nanoTime();
 				i++;
-			} else if (i >= 3 && TimeUtils.timeSinceNanos(startTime) > 1000000000) {
+			} else if (i >= 3 && TimeUtils.timeSinceNanos(countDownTime) > 1000000000) {
 				gameStarted = true;
+				raceStartTime = TimeUtils.nanoTime();
 				inPreRace = false;
-				gameState = 1;
+				gameState = 0;
 				velocity = 1;
-			} else if (i < 3 && TimeUtils.timeSinceNanos(startTime) < 1000000000) {
+			} else if (i < 3 && TimeUtils.timeSinceNanos(countDownTime) < 1000000000) {
 				batch.draw(lights[i], 522, 1440, Gdx.graphics.getWidth() / 4, Gdx.graphics.getHeight() / 4);
 			}
 		} else if (gameStarted) {
-			if (gameState == 1) {
+			if (gameState == 1) { // screen has been tapped, car is in motion
 				if (Gdx.input.justTouched()) {
 					numClicks++;
 					carY += velocity;
 					velocity+=.55;
 				}
-				if (carY < finishLine + finishDistance) {
+				if (carY < finishLine + finishDistance-25) {
 					velocity += drag;
 					carY += velocity;
 					setCarMovement();
 				} else {
 					gameState = 2;
 				}
-			} else if (gameState == 0) {
+			} else if (gameState == 0) { // user tapped once
 				if (Gdx.input.justTouched()) {
 					gameState = 1;
 				}
-			} else if (gameState == 2) {
-				//GAME OVER win winnnnderrr?
+			} else if (gameState == 2) { // game over
+				raceTime = (TimeUtils.timeSinceNanos(raceStartTime))/1000000000; // seconds ... how to get some decimal places? just concatinate a decimal?
+				System.out.println("This was the time it took to raceEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE " + raceTime);
+				stopShit();
 			}
 			batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 			batch.draw(finish, 655, 1280, Gdx.graphics.getWidth() / 11, Gdx.graphics.getWidth() / 12);
-			batch.draw(car, (Gdx.graphics.getWidth() / 2 - carWidth/2), carY, carWidth, carHeight);
+			batch.draw(orangeCar, (Gdx.graphics.getWidth() / 2 - carWidth/2), carY, carWidth, carHeight);
 		}
 		batch.end();
 	}
-	
+
 	public void setCarMovement(){
-		System.out.println("CarY is " + (carY - 135));
 		deltaY = (carY - 135)*.0075f;
 		carWidth = width / (4.5f + ((deltaY*deltaY*deltaY)*.055f));
 		carHeight = height / (4.5f + ((deltaY*deltaY*deltaY)*.055f));
 	}
-
+	public void stopShit(){
+		System.out.println("This was the time it took to race... HAS IT ENDEDDDDDDDDDD " + raceTime);
+		gameStarted = false;
+	}
 		@Override
 		public void dispose () {
 			batch.dispose();
-			//	img.dispose();
 		}
 	}
 
